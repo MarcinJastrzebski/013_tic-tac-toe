32142ff0965893628a0549f4bb66fc3e935cf2ff
Marcin Jastrzębski <marcin.jastrzebski@outlook.com>
2020-05-14 00:27:52 +0200

    Complete Settings Class

--- src/com/kodilla/first/individual/project/Logic/Settings.java
+++ src/com/kodilla/first/individual/project/Logic/Settings.java
@@ -3,7 +3,8 @@ package com.kodilla.first.individual.project.Logic;
 import javafx.scene.image.Image;
 
 public final class Settings {
-    private static Settings settings =null;
+    private static Settings settings = null;
+
     public final static Integer EASY = 1;
     public final static Integer MEDIUM = 2;
     public final static Integer HARD = 3;
@@ -12,37 +13,31 @@ public final class Settings {
     public final static Image XLETTER = new Image("file:resources/x-letter.png");
     public final static Image OLETTERRED = new Image("file:resources/o-letter-red.png");
     public final static Image XLETTERRED = new Image("file:resources/x-letter-red.png");
+    public final static Image TRANSP = new Image("file:resources/transp.png");
 
     public final static String XLETTERSTRING = "Krzyżyk";
     public final static String OLETTERSTRING = "Kółko";
 
-
-    private final static Image TRANSP = new Image("file:resources/transp.png");
-    private final static String GRAJ = "Graj!";
-    private final static String RESET = "Nowa gra";
-
-
-    private boolean endGame;
+    private boolean endGame = true;
     private Image player = XLETTER;
     private Image playerRed = XLETTERRED;
     private Image computer = OLETTER;
     private Image computerRed = OLETTERRED;
     private Image startingFigure = XLETTER;
 
-
     private Integer oScore = 0;
     private Integer xScore = 0;
     private Integer numberOfPlayerMoves = 0;
     private Integer numberOfComputerMoves = 0;
     private Integer level = 1;
-    private Integer numberOfGames =3;
+    private Integer numberOfGames = 3;
 
     private Settings() {
     }
 
     public static Settings getInstance() {
         if (settings == null) {
-            synchronized(Settings.class) {
+            synchronized (Settings.class) {
                 if (settings == null) {
                     settings = new Settings();
                 }
@@ -51,12 +46,12 @@ public final class Settings {
         return settings;
     }
 
-    public Image getStartingFigure() {
-        return startingFigure;
+    public boolean isEndGame() {
+        return endGame;
     }
 
-    public void setStartingFigure(Image startingFigure) {
-        this.startingFigure = startingFigure;
+    public void setEndGame(boolean endGame) {
+        this.endGame = endGame;
     }
 
     public Image getPlayer() {
@@ -67,14 +62,6 @@ public final class Settings {
         this.player = player;
     }
 
-    public Image getComputer() {
-        return computer;
-    }
-
-    public void setComputer(Image computer) {
-        this.computer = computer;
-    }
-
     public Image getPlayerRed() {
         return playerRed;
     }
@@ -83,12 +70,28 @@ public final class Settings {
         this.playerRed = playerRed;
     }
 
+    public Image getComputer() {
+        return computer;
+    }
+
+    public void setComputer(Image computer) {
+        this.computer = computer;
+    }
+
     public Image getComputerRed() {
         return computerRed;
     }
 
     public void setComputerRed(Image computerRed) {
         this.computerRed = computerRed;
+    }
+
+    public Image getStartingFigure() {
+        return startingFigure;
+    }
+
+    public void setStartingFigure(Image startingFigure) {
+        this.startingFigure = startingFigure;
     }
 
     public Integer getoScore() {
@@ -139,8 +142,8 @@ public final class Settings {
         this.numberOfGames = numberOfGames;
     }
 
-    public String getNameOfImage(Image image){
-        if (XLETTER.equals(image)||XLETTERRED.equals(image)){
+    public String getNameOfImage(Image image) {
+        if (XLETTER.equals(image) || XLETTERRED.equals(image)) {
             return XLETTERSTRING;
         } else {
             return OLETTERSTRING;
